bdeb0d370a65045d10b08efdaace2aec7d0aa084
arun <arun.chan86@gmail.com>
2023-07-27 13:56:50 +0530

    19- Connect To MongoDB Database

--- backend/Server.js
+++ backend/Server.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app =express();
+
+dotenv.config();
 
 app.get('/api/products',(req,res) =>{
     res.send(data.products);
@@ -27,4 +31,11 @@ app.get('/api/products/:id',(req,res) =>{
 const port = process.env.PORT || 5000;
 app.listen (port,() =>{
     console.log(`server at http://localhost:${port}`)
+})
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>{
+    console.log('Connected DB')
+}).catch((err)=>{
+    console.log(err.message)
 })
