e2ab5c8c75341fb74261ce28ed836dd9b0186b30
arun <arun.chan86@gmail.com>
2023-07-27 18:06:46 +0530

    20-seed sample products

--- backend/Server.js
+++ backend/Server.js
@@ -2,32 +2,17 @@ import express from 'express'
 import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 const app =express();
 
 dotenv.config();
 
-app.get('/api/products',(req,res) =>{
-    res.send(data.products);
-});
+app.use('/api/seed',seedRouter);
+app.use('/api/products',productRouter)
 
-app.get('/api/products/slug/:slug',(req,res) =>{
-    const product = data.products.find(x =>x.slug === req.params.slug);
-    if(product){
-        return res.send(product)
-    }else{
-         res.status(404).send({message:'Product Not Found'})
-    }
-});
 
-app.get('/api/products/:id',(req,res) =>{
-    const product = data.products.find(x =>x._id === req.params.id);
-    if(product){
-        return res.send(product)
-    }else{
-         res.status(404).send({message:'Product Not Found'})
-    }
-});
 const port = process.env.PORT || 5000;
 app.listen (port,() =>{
     console.log(`server at http://localhost:${port}`)
