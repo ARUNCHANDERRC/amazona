e8390c8086fc4a25d631fdafe727ffd85cdebca2
arun <arun.chan86@gmail.com>
2023-07-28 15:42:51 +0530

    22- Create Signin Backend API

--- backend/Server.js
+++ backend/Server.js
@@ -4,14 +4,24 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app =express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 dotenv.config();
 
 app.use('/api/seed',seedRouter);
 app.use('/api/products',productRouter)
+app.use('/api/users',userRouter)
 
+/* Middleware */
+app.use((err,req,res,next)=>{
+  res.status(500).send({message:err.message})
+})
+/* */
 
 const port = process.env.PORT || 5000;
 app.listen (port,() =>{
