095eae491ba5c4b81be44c9d5ce806951eddbb0a
arun <arun.chan86@gmail.com>
2023-08-01 12:36:35 +0530

    23- Complete Sigin Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,44 +10,74 @@ import Nav from 'react-bootstrap/Nav';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
+import { NavDropdown } from 'react-bootstrap';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
-  const { state } = useContext(Store);
-  const {cart} =state;
+  const { state ,dispatch:ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
+
+  const signoutHandler = () =>{
+    ctxDispatch({type: 'USER_SIGNOUT'});
+    localStorage.removeItem('userInfo');
+  }
+
   return (
     <BrowserRouter>
-      <div className='d-flex flex-column site-container'>
+      <div className="d-flex flex-column site-container">
+       <ToastContainer position='bottom-center' limit={1} />
         <header>
           <Navbar bg="dark" variant="dark">
             <Container>
               <LinkContainer to="/">
                 <Navbar.Brand>amazona</Navbar.Brand>
               </LinkContainer>
-              <Nav className ="me-auto">
-                <Link to ="/cart" className='nav-link'>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
                   cart
                   {cart.cartItems.length > 0 && (
                     <Badge pill bg="danger">
-                    {cart.cartItems.reduce((a,c)=> a+c.quantity,0)}
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                     </Badge>
                   )}
-                  </Link>
+                </Link>
+                {userInfo ? (
+                  <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>User Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/orderhistory">
+                      <NavDropdown.Item>Order History</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Divider />
+                    <Link
+                      className="dropdown-item"
+                      to="#signout"
+                      onClick={signoutHandler}
+                    >
+                      Sign Out
+                    </Link>
+                  </NavDropdown>
+                ) : (
+                  <Link className="nav-link" to="/signin">Sign In</Link>
+                )}
               </Nav>
             </Container>
           </Navbar>
         </header>
         <main>
-          <Container className='mt-3'>
+          <Container className="mt-3">
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/" element={<HomeScreen />} />
-              <Route path="/cart" element={<CartScreen/>} />
-              <Route path="/signin" element={<SigninScreen/>} />
+              <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
             </Routes>
           </Container>
         </main>
         <footer>
-          <div className='text-center'>All rights reserved</div>
+          <div className="text-center">All rights reserved</div>
         </footer>
       </div>
     </BrowserRouter>
--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -30,12 +30,23 @@ function reducer(state, action) {
       localStorage.setItem('cartItems', JSON.stringify(cartItems)); //to avoid disappering when refreshing page
       return { ...state, cart: { ...state.cart, cartItems } };
     }
+
+    case 'USER_SIGNIN':
+      return { ...state, userInfo: action.payload };
+
+    case 'USER_SIGNOUT':
+      return { ...state, userInfo: null };
+
     default:
-      return state;     
+      return state;
   }
 }
 
 const initialState = {
+  userInfo:localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  :null,
+
   cart: {
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
