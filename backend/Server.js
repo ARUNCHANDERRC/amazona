6e992145b284c285f1e74de0ade23fe24143355d
arun <arun.chan86@gmail.com>
2023-08-09 21:18:04 +0530

    33 - Publish To Heroku

--- backend/Server.js
+++ backend/Server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import data from './data.js';
+import path from 'path';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
@@ -22,11 +22,18 @@ app.use('/api/seed',seedRouter);
 app.use('/api/products',productRouter);
 app.use('/api/users',userRouter);
 app.use('/api/orders',orderRouter)
+
 /* Middleware */
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+
 app.use((err,req,res,next)=>{
   res.status(500).send({message:err.message})
 })
-/* */
+
 
 const port = process.env.PORT || 5000;
 app.listen (port,() =>{
